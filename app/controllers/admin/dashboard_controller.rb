826f79bfcc0961c83e4788fb67e00c92dba52eb6
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-07-05 17:25:15 -0400

    Implement code to ask for username and password before entering admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV["BASIC_AUTH_CREDENTIALS_USERNAME"], password: ENV["BASIC_AUTH_CREDENTIALS_PASSWORD"]
+
   def show
   end
 end
