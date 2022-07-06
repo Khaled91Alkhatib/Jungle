e5dcb469423aba966fe0b98bf200c90112d1617d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-07-06 14:07:39 -0400

    Add instance variables to check count of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["BASIC_AUTH_CREDENTIALS_USERNAME"], password: ENV["BASIC_AUTH_CREDENTIALS_PASSWORD"]
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
